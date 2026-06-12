de2920a9e33932c00eaab465925083a04c8e4b19
NotAShelf <raf@notashelf.dev>
2026-06-12 11:22:48 +0300

    treewide: fix clippy warnings
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Ic41cb7f76f29b27a8e7eec61427b91596a6a6964

--- crates/common/src/models.rs
+++ crates/common/src/models.rs
@@ -15,7 +15,7 @@ pub enum AuthKind {
 
 impl AuthKind {
   #[must_use]
-  pub fn as_str(self) -> &'static str {
+  pub const fn as_str(self) -> &'static str {
     match self {
       Self::Token => "token",
       Self::Oidc => "oidc",
